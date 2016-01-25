23bb1015e7f480a375626b9aa4e67bba276f5f2b
Michael Funk <mafunk92@gmail.com>
2016-01-24 17:52:07 -0800

    finished

---
+++ Exercises/SumOfPrimes.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // sum first 1000 primes;
+        int max = 1000;
+
+        // 2 is first prime, going to start with that
+        int count = 1;
+        int dx = 2;
+        int sum = 0;
+
+        while(count<=max)
+        {
+            bool isPrime = true;
+            for(int i=2;i <= Math.Floor(Math.Sqrt(dx));i++ )
+            {
+                if (dx % i == 0 && dx != i) isPrime = false;
+            }
+
+            if (isPrime)
+            {
+                sum += dx;
+                count++;
+            }
+            dx++;
+        }
+
+        Console.WriteLine(sum);     
+    }
+}
